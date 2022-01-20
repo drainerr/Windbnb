16bbd5f1c4abca1acaea29f3dd3a4afd60d30363
GeorgeKVR <george.kvrivi@gmail.com>
2022-01-20 15:30:37 +0400

    create and styled the  main container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './Components/Header/Header';
 import './App.css';
+import Main from './Components/Main/Main';
 
 function App() {
   const [chosenLocation, setChosenLocation] = useState('Helsinki, Finland');
@@ -10,6 +11,7 @@ function App() {
         chosenLocation={chosenLocation}
         setChosenLocation={setChosenLocation}
       />
+      <Main />
     </div>
   );
 }
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,6 @@
+import styles from './Main.module.css';
+const Main = (props) => {
+  return <div className={styles.mainContainer}></div>;
+};
+
+export default Main;
